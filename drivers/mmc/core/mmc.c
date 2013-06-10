babe7f34faa62efeef5f2a038b748dd76a527a18
Ulf Hansson <ulf.hansson@linaro.org>
2013-06-10 17:03:44 +0200

    mmc: core: Add shutdown callback for (e)MMC bus_ops
    The shutdown sequence of an (e)MMC is very similar to a suspend. We
    re-use the suspend function and tell it we are not in suspend context.
    
    Signed-off-by: Ulf Hansson <ulf.hansson@linaro.org>
    Signed-off-by: Chris Ball <cjb@laptop.org>
    Git-commit: eb91ee02bce94d96560b670508f64a86544f2ecc
    Git-repo: https://android.googlesource.com/kernel/common.git
    [imaund@codeaurora.org: Resolved context conflicts]
    Signed-off-by: Ian Maund <imaund@codeaurora.org>
    Signed-off-by: dev-harsh1998 <harshitjain6751@gmail.com>
    Signed-off-by: Ad!thya R <gh0strider.2k18.reborn@gmail.com>
    
    Signed-off-by: Mohd Faraz <mohd.faraz.abc@gmail.com>

--- drivers/mmc/core/mmc.c
+++ drivers/mmc/core/mmc.c
@@ -1893,6 +1893,14 @@ static int mmc_suspend(struct mmc_host *host)
 }
 
 /*
+ * Shutdown callback
+ */
+static int mmc_shutdown(struct mmc_host *host)
+{
+	return _mmc_suspend(host, false);
+}
+
+/*
  * Resume callback from host.
  *
  * This function tries to determine if the same card is still present
@@ -2017,6 +2025,7 @@ static const struct mmc_bus_ops mmc_ops = {
 	.power_restore = mmc_power_restore,
 	.alive = mmc_alive,
 	.change_bus_speed = mmc_change_bus_speed,
+	.shutdown = mmc_shutdown,
 };
 
 static const struct mmc_bus_ops mmc_ops_unsafe = {
@@ -2029,6 +2038,7 @@ static const struct mmc_bus_ops mmc_ops_unsafe = {
 	.power_restore = mmc_power_restore,
 	.alive = mmc_alive,
 	.change_bus_speed = mmc_change_bus_speed,
+	.shutdown = mmc_shutdown,
 };
 
 static void mmc_attach_bus_ops(struct mmc_host *host)
