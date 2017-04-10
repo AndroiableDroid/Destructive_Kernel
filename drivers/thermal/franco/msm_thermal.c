6770cd5570cd86aec67acea0efae1097fb1ca0af
Shoaib0597 <Shoaib0595@gmail.com>
2017-04-11 01:57:04 +0530

    Introduce Shoaib's Core Control v1.0
    Signed-off-by: Shoaib0597 <Shoaib0595@gmail.com>
    
    Although, I got Franco's Thermal Driver to work, there was still one thing missing in the Thermal Department and that was, a fully Automated, Temperature-Based, Core-Control Driver. Hence, I started working on this too. I paid proper attention to the mechanism of HotPlugging Cores and after many hours of tweaking and testing, I finally managed to develop the best way to HotPlug Cores based on Temperature. After I got my Core-Control Driver working, I merged this Driver of mine into Franco's Thermal Driver's Source Code to keep things tidy and clean. This Driver is now called as Shoaib's Core Control.
    
    The Core-Disabling Mechanism of Shoaib's Core-Control is explained below---
    For big.LITTLE Octa-Core SoCs---
    Temperature (Celsius)   Cores (Numbering starts from 0)
    at 40			All Cores Online
    between 46 and 50       Disable Cores 2 and 3
    between 56 and 65	Disable the big Cluster i.e., Core 0, 1, 2 and 3
    above 80		Disable Cores 0, 1, 2, 3, 6 and 7
    
    For Normal Quad-Core SoCs---
    Temperature (Celsius)   Cores (Numbering starts from 0)
    at 40			All Cores Online
    between 46 and 50	Disable Core 3
    between 56 and 65       Disable Core 2
    above 80		Disable Core 1
    
    The best part about Shoaib's Core-Control is that it can detect whether it is running on a big.LITTLE SoC or on a Traditional Quad-Core SoC and configure itself accordingly. Further, Shoaib's Core-Control can even disable the big Cluster completely if the need arises to cool the device quickly.
    
    Dependancies---
    1. AiO_HotPlug---
    To avoid conflicts, I have intentionally added a foolproof mechanism which restricts the users from using Shoaib's Core Control with AiO_HotPlug.
    2. HotPlug Boost Toggle---
    To give users control over the Enabling/Disabling of Core 0, I used HotPlug Boost Toggle as a Permission Toggle. If it is on, then only Core 0 can be Disabled otherwise Core 0 will always be Online.

--- drivers/thermal/franco/msm_thermal.c
+++ drivers/thermal/franco/msm_thermal.c
@@ -9,7 +9,10 @@
  * 3. Changed the Default Values to more Efficient Parameters for Better Heat-Management and Battery-Life.
  * 4. Switched to Power Efficient WorkQueues for lesser footprint on CPU.
  * 5. Removed Two Frequency Throttle Points, only two are now available as against four (four are unnecessary as well as having only two also reduces calculations making the Driver lighter).
- * 6. Altered the Formatting of the Codes (looks cleaner and more beautiful now). 
+ * 6. Added a function to allow users to configure whether Core 0 should be disabled or not.
+ * 7. Added a check to make sure that Frequency Input from the user is only taken when Permission to Disable Core has not been granted even once (for big.LITTLE SoC) to prevent freezes.
+ * 8. Introduced Shoaib's Core Control, an Automatic HotPlug based on Temperature.
+ * 9. Altered the Formatting of the Codes (looks cleaner and more beautiful now).
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -49,6 +52,25 @@ int FREQ_WARM 		= 1113600;
 #else
 int FREQ_WARM 		= 1094400;
 #endif
+
+#ifdef CONFIG_CORE_CONTROL
+// Essentials for Shoaib's Core Control.
+bool core_control = true;
+static struct kobject *cc_kobj;
+#endif
+
+#ifdef CONFIG_AiO_HotPlug
+extern int AiO_HotPlug;
+#endif
+
+#if (NR_CPUS == 8)	// Assume Octa-Core SoCs to be based on big.LITTLE architecture.
+// Permission to Disable Core 0 Toggle.
+extern bool hotplug_boost;
+#endif
+
+// Variables to know the user-behaviour.
+int flag = 0;
+int count = 0;
 
 /* Temperature Threshold Storage */
 static int set_temp_threshold(const char *val, const struct kernel_param *kp)
@@ -112,14 +134,18 @@ static int set_freq_limit(const char *val, const struct kernel_param *kp)
 	static struct cpufreq_frequency_table *tbl = NULL;
 	
 	ret = kstrtouint(val, 10, &i);
-        
-	if (ret)
+
+	// Don't store any values if permission to disable Core 0 has been granted once on a big.LITTLE SoC to avoid the situation where the whole system freezes. To again use this function, the phone must be rebooted.
+	if (ret || count == 1)
 	   return -EINVAL;
- 
-	policy = cpufreq_cpu_get(0);
+
+        policy = cpufreq_cpu_get(0);
 	tbl = cpufreq_frequency_get_table(0);
 
 	ret = param_set_int(val, kp);
+
+	// Set flag to 1 if Thermal Frequency Table have been changed by user.
+	flag = 1;
 
 	return ret;
 }
@@ -197,6 +223,123 @@ static void __ref check_temp(struct work_struct *work)
 	tsens_dev.sensor_num = msm_thermal_info.sensor_id;
 	tsens_get_temp(&tsens_dev, &temp);
 
+	// Switch on Core 0 again as soon as Permission to disable it is denied.
+	if (!cpu_online(0))
+	{
+	   count = 1;
+	   if (hotplug_boost == false)
+	      cpu_up(0);
+	}
+
+	#ifdef CONFIG_CORE_CONTROL
+	// Begin HotPlug Mechanism for Shoaib's Core Control
+	if (core_control)
+	{
+	   // If SoC is an Octa-Core one, assume it to be based on big.LITTLE.
+	   if (NR_CPUS == 8)
+	   {
+	      if (temp > 80)
+	      {
+	         if (cpu_online(3))
+	      	    cpu_down(3);
+	         if (cpu_online(2))
+	            cpu_down(2);
+	         if (cpu_online(1))
+	            cpu_down(1);
+		 // Disable Core 0 only if Permission is granted and Thermal Frequency Table has not been changed by user.
+	         if (hotplug_boost == true && flag == 0)
+		 {
+	            if (cpu_online(0))
+	               cpu_down(0);
+		 }
+	         if (cpu_online(7))
+	            cpu_down(7);
+	         if (cpu_online(6))
+	            cpu_down(6); 
+	      }
+	      else if (temp > 55 && temp <= 65)
+	      {
+		      if (!cpu_online(6))
+		         cpu_up(6);
+	              if (!cpu_online(7))
+	 	         cpu_up(7);
+
+	              if (cpu_online(3))
+	      	         cpu_down(3);
+	              if (cpu_online(2))
+	                 cpu_down(2);
+		      if (cpu_online(1))
+	                 cpu_down(1);
+		      // Disable Core 0 only if Permission is granted and Thermal Frequency Table has not been changed by user.
+		      if (hotplug_boost == true && flag == 0)
+		      {
+	              if (cpu_online(0))
+	                 cpu_down(0);
+		      }
+		      
+	      }
+	      else if (temp > 45 && temp <= 50)
+	      {
+		      if (!cpu_online(0))
+	                 cpu_up(0);
+	              if (!cpu_online(1))
+	                 cpu_up(1);
+	          
+	              if (cpu_online(3))
+	                 cpu_down(3);
+	              if (cpu_online(2))
+	                 cpu_down(2);
+	      }
+	      else if (temp == 40)
+	      {
+	              int cpu;
+	              for_each_possible_cpu(cpu)
+	                  if (!cpu_online(cpu))
+		             cpu_up(cpu);
+	      }
+	   }
+	   // If SoC is a Quad-Core one, assume it to be of Traditional Configuration.
+	   else if (NR_CPUS == 4)
+	   {
+	           if (temp > 80)
+	           {
+	           if (cpu_online(3))
+	      	      cpu_down(3);
+	           if (cpu_online(2))
+	              cpu_down(2);
+	           if (cpu_online(1))
+	              cpu_down(1); 
+	           }
+	           else if (temp > 70 && temp <= 75)
+	           {
+			   if (!cpu_online(1))
+	                      cpu_up(1);
+
+	                   if (cpu_online(3))
+	      	              cpu_down(3);
+	                   if (cpu_online(2))
+	                      cpu_down(2);
+	           }
+	           else if (temp > 60 && temp <= 65)
+	           {
+	                   if (!cpu_online(2))
+	                      cpu_up(2);
+
+	                   if (cpu_online(3))
+	                      cpu_down(3);
+	           }
+	           else if (temp == 55)
+	           {
+	                   int cpu;
+	                   for_each_possible_cpu(cpu)
+	                       if (!cpu_online(cpu))
+		                  cpu_up(cpu);
+	           }
+	   }
+        }
+	// End HotPlug Mechanism for Shoaib's Core Control
+	#endif
+
 	if (info.throttling) 
         {
 	   if (temp < (TEMP_THRESHOLD - info.safe_diff)) 
@@ -223,6 +366,103 @@ static void __ref check_temp(struct work_struct *work)
 reschedule:
 	queue_delayed_work(system_power_efficient_wq, &check_temp_work, msecs_to_jiffies(1000));
 }
+
+#ifdef CONFIG_CORE_CONTROL
+// Begin sysFS for Shoaib's Core Control
+static ssize_t show_cc_enabled(struct kobject *kobj, struct kobj_attribute *attr, char *buf)
+{
+	return snprintf(buf, PAGE_SIZE, "%d\n", core_control);
+}
+
+static ssize_t __ref store_cc_enabled(struct kobject *kobj, struct kobj_attribute *attr, const char *buf, size_t count)
+{
+	int ret = 0;
+	int val = 0;
+
+	ret = kstrtoint(buf, 10, &val);
+	#ifdef CONFIG_AiO_HotPlug
+	// Allow Shoaib's Core Control to be Enabled only if AiO HotPlug is Disabled.
+	if (ret || AiO_HotPlug == 1) 
+	{
+	   pr_err("Invalid input %s. err:%d\n", buf, ret);
+	   goto done_store_cc;
+	}
+	#else
+	if (ret)
+	{
+	   pr_err("Invalid input %s. err:%d\n", buf, ret);
+	   goto done_store_cc;
+	}
+	#endif
+	
+
+	if (core_control == !!val)
+	   goto done_store_cc;
+
+	core_control = !!val;
+
+	if (!core_control)
+	{
+	   int cpu;
+	   /* Wake-Up All the Sibling Cores */
+	   for_each_possible_cpu(cpu)
+	       if (!cpu_online(cpu))
+		  cpu_up(cpu);
+	}
+
+done_store_cc:
+	return count;
+}
+
+static __refdata struct kobj_attribute cc_enabled_attr = 
+__ATTR(core_control, 0644, show_cc_enabled, store_cc_enabled);
+
+static __refdata struct attribute *cc_attrs[] = {
+	&cc_enabled_attr.attr,
+	NULL,
+};
+
+static __refdata struct attribute_group cc_attr_group = {
+	.attrs = cc_attrs,
+};
+
+static __init int msm_thermal_add_cc_nodes(void)
+{
+	struct kobject *module_kobj = NULL;
+	int ret = 0;
+
+	module_kobj = kset_find_obj(module_kset, KBUILD_MODNAME);
+	if (!module_kobj) 
+	{
+	   pr_err("cannot find kobject\n");
+	   ret = -ENOENT;
+	   goto done_cc_nodes;
+	}
+
+	cc_kobj = kobject_create_and_add("core_control", module_kobj);
+	if (!cc_kobj) 
+	{
+	   pr_err("cannot create core control kobj\n");
+	   ret = -ENOMEM;
+	   goto done_cc_nodes;
+	}
+
+	ret = sysfs_create_group(cc_kobj, &cc_attr_group);
+	if (ret) 
+	{
+	   pr_err("cannot create sysfs group. err:%d\n", ret);
+	   goto done_cc_nodes;
+	}
+
+	return 0;
+
+done_cc_nodes:
+	if (cc_kobj)
+	   kobject_del(cc_kobj);
+	return ret;
+}
+// End sysFS for Shoaib's Core Control
+#endif
 
 static int msm_thermal_dev_probe(struct platform_device *pdev)
 {
@@ -271,6 +511,12 @@ static struct platform_driver msm_thermal_device_driver = {
 
 static int __init msm_thermal_device_init(void)
 {
+	#ifdef CONFIG_CORE_CONTROL
+	// Initialize Shoaib's Core Control Driver 
+	if (num_possible_cpus() > 1)
+	   msm_thermal_add_cc_nodes();
+	#endif
+
 	return platform_driver_register(&msm_thermal_device_driver);
 }
 
