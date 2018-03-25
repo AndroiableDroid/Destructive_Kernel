7450a212eb777ef5dea86fc67e0a20a304b93c1a
Sultanxda <sultanxda@gmail.com>
2018-03-25 15:25:41 +0000

    kernel: Remove hacks to force kernel tasks onto little CPU cluster
    Signed-off-by: Francisco Franco <franciscofranco.1990@gmail.com>

--- kernel/workqueue.c
+++ kernel/workqueue.c
@@ -1490,7 +1490,7 @@ static void __queue_delayed_work(int cpu, struct workqueue_struct *wq,
 	timer->expires = jiffies + delay;
 
 	if (unlikely(cpu != WORK_CPU_UNBOUND))
-		add_timer_on(timer, 0);
+		add_timer_on(timer, cpu);
 	else
 		add_timer(timer);
 }
