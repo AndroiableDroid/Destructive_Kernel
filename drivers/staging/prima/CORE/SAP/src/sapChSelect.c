2b81cc1484ed6eb400907a6c9e1a049ff941b494
dev-harsh1998 <dev-harsh1998@hotmail.com>
2018-09-08 02:34:56 +0530

    GCC-8: we back to the show!
    This reverts commit df665e281ac73c1f1295a4fa1937c4f6ff44c4c7.
    
    Signed-off-by: Mohd Faraz <mohd.faraz.abc@gmail.com>

--- drivers/staging/prima/CORE/SAP/src/sapChSelect.c
+++ drivers/staging/prima/CORE/SAP/src/sapChSelect.c
@@ -1454,6 +1454,7 @@ void sapComputeSpectWeight( tSapChSelSpectInfo* pSpectInfoParams,
     tSapSpectChInfo *pSpectChStartAddr = pSpectInfoParams->pSpectCh;
     tSapSpectChInfo *pSpectChEndAddr =
                     pSpectInfoParams->pSpectCh + pSpectInfoParams->numSpectChans;
+    tSapSpectChInfo *pExtSpectCh = NULL;
 
     pBeaconStruct = vos_mem_malloc(sizeof(tSirProbeRespBeacon));
     if ( NULL == pBeaconStruct )
